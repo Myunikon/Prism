7d0523f1e958a4802100d2787cdb3d10a1e2b053
Myunikon <farraznawwaf123@gmail.com>
2025-12-19 13:56:46 +0800

    Fix PWA Icons: Move to public and update path

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: "assets/icon.png", // Vite PWA handles asset pathing
+            src: "icon.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "assets/icon.png",
+            src: "icon.png",
             sizes: "512x512",
             type: "image/png",
           },
