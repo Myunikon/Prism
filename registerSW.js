899116950c89582f54a1810549578c018505f3a5
Myunikon <farraznawwaf123@gmail.com>
2025-12-19 13:33:51 +0800

    Fix 404s: Relative base path and .nojekyll

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Prism/sw.js', { scope: '/Prism/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
